e4bc17ef7e47fbd8ddcc50ce159b5b329ac4f166
Job Jean-Pierre Joseph <jobjpjoseph@gmail.com>
2023-11-26 05:28:58 -0500

    We finished the assignment.

--- classes/student.js
+++ classes/student.js
@@ -1,5 +1,30 @@
 const Person = require('./person');
 
+class Student extends Person {
+  constructor(firstName, lastName, major, GPA) {
+    super(firstName, lastName);
+    this.major = major;
+    this.GPA = GPA;
+  }
+
+  static printGPA(student) {
+    return `${student.firstName} ${student.lastName} has the higher GPA.`;
+  }
+  static compareGPA(student1, student2) {
+    const [GPA1 , GPA2] = [student1.GPA, student2.GPA];
+
+    if (GPA1 > GPA2) {
+      return this.printGPA(student1);
+    } else if (GPA2 > GPA1) {
+      return this.printGPA(student2);
+    } else {
+      return `Both students have the same GPA`;
+    }
+
+  }
+
+}
+
 // Your code here
 
 /****************************************************************************/
--- classes/teacher.js
+++ classes/teacher.js
@@ -1,6 +1,22 @@
 const Person = require('./person');
 
 // Your code here
+class Teacher extends Person {
+  constructor(firstName, lastName, subject, yearsOfExperience) {
+    super(firstName, lastName);
+    this.subject = subject;
+    this.yearsOfExperience = yearsOfExperience;
+  }
+
+  static combinedYearsOfExperience(teachers) {
+    return teachers.reduce((accum, teacher) => {
+      return accum + teacher.yearsOfExperience;
+      // Note: 0 is not an instance.
+      // 0 is the intial value so === accum
+    }, 0);
+  }
+
+}
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
